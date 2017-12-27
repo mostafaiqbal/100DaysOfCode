5648a5020b3999b98f1013d352bdb08c4b361298
rasheledu <rashel.edu@gmail.com>
2017-12-27 22:34:07 +0000

    Day01 updates

---
+++ Day01.js
@@ -0,0 +1,50 @@
+
+
+
+
+
+/*
+//Free Code Camp Challenge: Repeat a string repeat a string
+function repeatStringNumTimes(str, num) {
+
+    if (num <= 0) {
+
+        return "";
+    } else {
+
+        return str.repeat(num);
+    }
+}
+console.log(
+repeatStringNumTimes("abc", -2) );
+
+
+// Free Code Camp Challenge : Confirm the Ending
+
+    function confirmEnding(str, target) {
+
+        var checkpoint = str.length - target.length;
+    return    Boolean (str.substring(checkpoint) == target)
+    }
+    console.log(    confirmEnding("Bastian", "n"));
+
+
+// Free Code Camp Challenge : Retrun Largest Number Array
+
+    function largestOfFour(arr) {
+        var newArray = [0,0,0,0]
+
+for (var i = 0; i < arr.length; i++) {
+  for (var y = 0; y < arr[i].length; y++ ) {
+
+      if (arr[i][y] > newArray[i]) {
+
+          newArray[i] = arr[i][y]
+      }
+  }
+}
+    return newArray;
+}
+console.log(
+largestOfFour ([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]])
+); */
