19271e7aa52c23a15bc20c87a7d3ceed6707e05b
rasheledu <rashel.edu@gmail.com>
2017-12-30 18:59:30 +0000

    Day - 2 Completed

--- Day02.js
+++ Day02.js
@@ -1,10 +1,69 @@
 // Day 02 - 30/12/2017
 
 
+// Free Code Camp Challenge: Caesars Cipher
+
+function rot13(str) {
+    var result = [];
+
+    for (var i = 0; i<str.length; i++ ) {
+            var num = str.charCodeAt(i);
+
+            if (num >= 65 && num <= 77) {
+                num += 13;
+            } else if (num >= 78 && num <= 90) {
+
+                num -=13;
+            }
+
+            result.push(String.fromCharCode(num));
+    }
+    return result.join("");
+}
+
+console.log(
+    rot13("SERR PBQR PNZC")
+);
 
 
 
-/**
+
+// Free Code Camp Challenge: Seek and Destroy
+function destroyer(arr, num) {
+    var arg = Array.prototype.slice.call(arguments, 1)
+    var newArray= arr.filter(function(item) {
+        return !arg.includes(item);
+
+    })
+    return newArray;
+}
+console.log(
+    destroyer([1, 2, 3, 1, 2, 3], 2, 3)
+);
+
+// Free Code Camp Challenge: Where do I belong
+
+    function getIndexToIns(arr, num) {
+        arr.push(num)
+        arr.sort(function(a,b){
+            return a - b;
+        })
+        return arr.indexOf(num)
+    }
+
+console.log(
+getIndexToIns([10, 20, 30, 40, 50], 30) );
+
+// Free Code Camp Challenge: Bouncer
+
+function bouncer(arr) {
+    return arr.filter(function(itemObject){
+            return itemObject;
+    })
+}
+console.log (
+bouncer([7, "ate", "", false, 9]) );
+
 //Free Code Camp Challenge: Mutations
 function mutation(arr) {
     var string1 = arr[0].toLowerCase();
