919e8fd2380d6bd8bbe6a0db5f2d8b27f65ac946
rasheledu <rashel.edu@gmail.com>
2017-12-30 22:23:43 +0000

    DAY - 3

---
+++ Day03.js
@@ -0,0 +1,30 @@
+// Day 03 - 31/12/2017
+
+// Free Code Camp Challenge: Object Oriented and Functional Programming (REVISE)
+
+function factorialize(num) {
+
+    for (var i = 0; i < num.length; i++) {
+
+        num *= i
+    }
+    return num ;.ilobyui8vbuy kli
+
+}
+console.log(
+    factorialize(5)
+);
+
+
+
+
+/*
+function reverseString(str) {
+
+    return str.split("").reverse().join("")
+}
+
+console.log(
+    reverseString("ydwoH")
+);
+*/
