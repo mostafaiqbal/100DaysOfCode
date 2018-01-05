da0574244aa89f85ef216b75504937e0fe7d1ba6
rasheledu <rashel.edu@gmail.com>
2018-01-05 12:04:56 +0000

    DAY 7 - INITIATED

---
+++ Day07.js
@@ -0,0 +1,76 @@
+// Day 07 - 05/01/2018
+// FreeCodeCamp Challenge: sObject Oriented and Functional Programming  (REVISE)
+
+//Array .reduce Method
+
+var array = [4,5,6,7,8];
+var singleVal = 0;
+
+
+singleVal = array;
+
+/*
+// Reformating object using map
+var kvArray = [{key: 1, value: 10},
+               {key: 2, value: 20},
+               {key: 3, value: 30}];
+
+var reformatedObject = kvArray.map (function(val) {
+
+    var newVal = {};
+    newVal[val.key] = val.value;
+
+    return newVal;
+
+})
+
+console.log(
+    reformatedObject
+);
+
+
+// .map method for ARRAY
+var oldArray = [1,2,3,4,5];
+
+var newArray = oldArray.map(function(val) {
+    return val + 3;
+});
+console.log(
+    newArray
+);
+
+
+//Object function
+var Car = function(wheels, engines, seats) {
+  this.wheels = wheels;
+  this.engines = engines;
+  this.seats = seats;
+};
+var myCar = new Car (8, 2, 10 );
+console.log(
+    myCar
+);
+//Make Instances of Objects with a Constructor Function
+var Car = function() {
+  this.wheels = 4;
+  this.engines = 1;
+  this.seats = 5;
+};
+var myCar = new Car();
+myCar.nickname = "Ferrari";
+console.log(
+    myCar
+);
+
+
+// Objcet
+
+var carObject = {
+"wheels" : 4,
+"Seats" : 5,
+"Engeen": 1,
+"SideGlass": 2,
+}
+
+console.log(carObject);
+*/
