adc3f0f50730d5ae4a39f6e07d043c096df45844
rasheledu <rashel.edu@gmail.com>
2018-01-06 23:52:53 +0000

    DAY 08

--- Day08.js
+++ Day08.js
@@ -1,2 +1,29 @@
 // Day 08 - 06/01/2018
 // FreeCodeCamp Challenge: sObject Oriented and Functional Programming  (REVISE)
+
+//array.join()
+var joinMe = ["Split","me","into","an","array"];
+var joinedString = '';
+joinedString = joinMe.join(' ');
+console.log(joinedString);
+
+
+//array.split()
+var string = "Split me into an array";
+var array = [];
+array = string.split(' ')
+console.log(array);
+
+//array.concat()
+var oldArray = [1,2,3];
+var newArray = [];
+var concatMe = [4,5,6];
+newArray = oldArray.concat(concatMe)
+console.log(newArray);
+
+
+//ARRY.Revese ()
+var array = [1,2,3,4,5,6,7];
+var newArray = [];
+    newArray = array.reverse()
+console.log(newArray);
