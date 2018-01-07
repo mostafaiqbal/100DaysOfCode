bd627578b103c250c0e0e6f006ebde6ac11499c2
rasheledu <rashel.edu@gmail.com>
2018-01-07 15:22:21 +0000

    DAY 08 - COMPLETED

--- Day08.js
+++ Day08.js
@@ -1,6 +1,54 @@
 // Day 08 - 06/01/2018
 // FreeCodeCamp Challenge: sObject Oriented and Functional Programming  (REVISE)
 
+
+
+//Find the LogestWord
+
+console.log(
+    findLongestWord("May the force be with you" )
+);
+
+
+
+// palindrome
+function palindrome(str) {
+    str = str.toLowerCase().replace(/[\W_]/g, "");
+    var reverseString = str.split("").reverse().join("")
+
+    if (reverseString === str) {
+        return true ;
+    } else {
+        return false;
+    }
+}
+
+console.log(
+    palindrome("1 eye for of 1 eye.")
+);
+
+
+//factorialize
+function factorialize(num) {
+    if ( num <= 1) {
+        return 1;
+    } else {
+        return num * factorialize(num - 1);
+    }
+}
+console.log(
+    factorialize(20)
+);
+
+//Reverse A String
+function reverseString(str) {
+return str.split('').reverse().join("");
+
+}
+console.log(
+    reverseString("Bangladesh")
+);
+
 //array.join()
 var joinMe = ["Split","me","into","an","array"];
 var joinedString = '';
