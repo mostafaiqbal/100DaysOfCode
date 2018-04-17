fef45b43269a5a54b8745be9a853cf4d122b67e4
rasheledu <rashel.edu@gmail.com>
2018-04-17 13:17:43 +0100

    update

--- Day01.js
+++ Day01.js
@@ -1,2 +1,9 @@
 
 // Day 01 - 12/04/2018
+
+document.getElementById("btn").onclick = function() {
+    document.getElementById("demo").innerHTML = "NOW YOU CAN SEE ME ";
+}
+document.getElementById("btn").ondblclick = function() {
+    document.getElementById("demo").innerHTML = "Love me";
+}
