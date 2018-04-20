4a632fdc09d304c70269a90c5f890165f3d88b77
rasheledu <rashel.edu@gmail.com>
2018-04-20 22:07:47 +0100

    index

--- Day01.js
+++ Day01.js
@@ -3,22 +3,60 @@
 
 //FreeCodeCamp Object Oriented and Functional Programming
 
+/*
 
 // #01 Declare JavaScript Objects as Variables 
 
-var car = {
-    "wheels":4,
-    "engines":1,
-    "seats":5
-  };
-  
-  var motorBike = 
+    var car = {
+        "wheels":4,
+        "engines":1,
+        "seats":5,
+    };
+    
+    var motorBike = {
+        "wheels" : 2,
+        "engines": 1,
+        "seats" : 2,
 
-  
-  };
+    };
+
+ 
+// #02 Instruct JavaScript Objects with Functions
+
+    var MotorBike = function() {
+        this.wheels = 2;
+        this.engines = 1;
+        this.seats = 2;
+    }
+
+    console.log(MotorBike);
+
+   // #03 Make Instances of Objects with a Constructor Function
+
+    var Car = function() {
+        this.wheels = 4;
+        this.engines = 1;
+        this.seats = 5;
+        };
+
+    var myCar = new Car ();
+        myCar.nickname = "TOYOTA";
+        console.log(myCar)    
 
 
-/*
 
+// #04 Make Unique Objects by Passing Parameters to our Constructor
+
+    var Car = function(wheels, seats, engines) {
+        //Change this constructor
+        this.wheels = wheels;
+        this.seats = seats;
+        this.engines = engines;
+    };
+    
+    
+    var myCar= new Car(4, 5, 2);
+    
+    console.log(myCar)
 
 */
