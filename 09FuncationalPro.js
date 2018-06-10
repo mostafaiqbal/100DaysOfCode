bc6e9657334d19827df6508b23e8cbc6c08c4ab6
M IQBAL <rashel.edu@gmil.com>
2018-06-10 19:48:20 +0100

    sunday

--- 09FuncationalPro.js
+++ 09FuncationalPro.js
@@ -1,1 +1,26 @@
 // FCC Funcational Programming
+
+let greenTea = "greenTea";
+let blackTea = "blackTea";
+
+const getTea = (typeOfTea, numberOfTeas) => {  
+    const teaCups = []; 
+    for (let blackTeaCup = 1; blackTeaCup <= numberOfTeas; blackTeaCup++) {
+        
+        teaCups.push(blackTea);
+    }
+
+    for (let greenTeaCup = 1; greenTeaCup <= numberOfTeas; greenTeaCup++) {
+        
+        teaCups.push(greenTea);
+    }
+
+
+
+    return teaCups;
+}
+
+const tea4GreenTeamFCC = getTea(greenTea, 40);
+
+console.log(tea4GreenTeamFCC); 
+
