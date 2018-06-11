8ede372b905ad021d93b912262127a074b11b406
M Iqbal <rashel.edu@gmail.com>
2018-06-11 15:40:24 +0100

    11/06

--- 10FCCES6.js
+++ 10FCCES6.js
@@ -1,1 +1,13 @@
-// ES6
+// FCC ES6
+let printNumberTwo;
+
+for (let i = 0; i < 3; i++) {
+    if (i === 2 ) {
+     printNumberTwo = function() {
+        return i;
+    };
+ }
+
+}
+
+console.log(printNumberTwo());
