7251b7b64f34dbf12f14ffa4a5e3d0246fefda23
M Iqbal <rashel.edu@gmail.com>
2018-07-02 22:03:31 +0100

    2/07

--- 10FCCES6.js
+++ 10FCCES6.js
@@ -35,10 +35,83 @@ const realNumberArray = [4, 5.6, -9.8, 3.14, 42, 6, 8.34];
 const squaredIntegers = realNumberArray.filter((val) => Number.isInteger(val)).map((val) => val*= val);
 
 console.log(squaredIntegers);
+ 
+   // Rest Parameter
+
+ function sumUp (...arg) {
+     console.log(arg)
+     let result = 0;
+     for (let i = 0; i < arg.length; i++) {        
+        result += arg[i];     
+     }
+     return result;
+ }
+console.log (sumUp(10,40,30));
+
+// Spread Operator 
+
+let number = [1,2,3,4,5];
+
+console.log(number);
+
+console.log(...number);
+
+
+// Template Literals 
+
+let name = "Gabrial";
+
+let greet = `Hello, I am ${name}`;
+console.log(greet);
+
+
+
+// De-Structuring
+
+let number = [1,2,3,4,5];
+
+//let[a,b] = number;
+//console.log (a, b);
+
+//[a, b] = [b, a];
+//console.log(a,b);
+let [a, ...b] = number;
+console.log(b);
+
+*/
+
+//Use class Syntax to Define a Constructor Function
+/**
+ * var spaceShuttle = function (targetPlanet) {
+    this.targetPlanet = targetPlanet;
+}
+
+var zeus = new spaceShuttle ("Jupiter");
+
+console.log(zeus);
+
+
+class spaceShuttle {
+    constructor (targetPlanet) {
+        this.targetPlanet = targetPlanet;
+    }
+}
+const mars = new spaceShuttle ("spaceX");
+
+console.log(mars);
  */
 
-let myString = "Hello, World!";
-let myRegex = /Hello/;
-console.log(
- myRegex.test(myString)
-)
+function makeClass() {
+  
+    /* Alter code below this line */
+  class Vegetable {
+    constructor () {
+      this.name = name;
+    }
+  }
+    /* Alter code above this line */
+    return Vegetable;
+  }
+  const Vegetable = makeClass();
+  const carrot = new Vegetable('carrot');
+  console.log(carrot.name); // => should be 'carrot'
