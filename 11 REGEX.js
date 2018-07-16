e926a4b3fe65a67e7a59b8a5245d2732a269f11c
M Iqbal <rashel.edu@gmail.com>
2018-07-16 18:55:33 +0100

    costa

--- 11 REGEX.js
+++ 11 REGEX.js
@@ -1,6 +1,15 @@
 //Regular Expression. 
-
+/**
+ 
 let myString = "Hello, World!";
 let myRegex = /Hello/;
  console.log(myRegex.test(myString)); // Change this line
+ */
+
+ /** We have 24 hours customer sarvie. 
+  If you are out of the UK, you can call us on +44-208-123-6763.
+  If you are from the UK please call us on 208-123-6763.
+  Alternatively, reach us on What's App messanger on +44-745-806-7617.
+
+  */
 
