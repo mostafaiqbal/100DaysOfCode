001926b139a5c78f42c7ca8e69bab795377fc640
M IQBAL <rashel.edu@gmil.com>
2018-07-17 00:23:25 +0100

    commit

---
+++ 11Regex.js
@@ -0,0 +1,7 @@
+// Regex
+/* We have 24 hour customer service line. 
+If you are from out side the UK please call us on 0044-208-123-6763.
+If you ar from UK please call us on 0208-123-6763.
+Alternatively please message us on what's app 0044-754-806-7617
+
+*/
