d920b28f221beaaa094fdae92d553c3a5d9c4119
M IQBAL <rashel.edu@gmil.com>
2018-07-19 14:07:35 +0100

    18th JULY

--- 11Regex.js
+++ 11Regex.js
@@ -1,7 +1,47 @@
 // Regex
-/* We have 24 hour customer service line. 
+/*
+We have 24 hour customer service line. 
 If you are from out side the UK please call us on 0044-208-123-6763.
 If you ar from UK please call us on 0208-123-6763.
-Alternatively please message us on what's app 0044-754-806-7617
+Alternatively please message us on what's app 0044.754.806.7617
+*/
+/*
+
+//Regular Expressions: Using the Test Method
+let myString = "Hello, World!";
+let myRegex = /Hello/;  
+let result = myRegex.test(myString);
+console.log(result);
+
+//Regular Expressions: Match Literal Strings
+let waldoIsHiding = "Somewhere Waldo is hiding in this text.";
+let waldoRegex = /Waldo/; // Change this line
+let result = waldoRegex.test(waldoIsHiding);
+console.log(result);
+
+//Regular Expressions: Match a Literal String with Different Possibilities
+let petString = "James has a pet cat.";
+let petRegex = /dog|cat|bird|fish/; // Change this line
+let result = petRegex.test(petString);
+
+//Regular Expressions: Ignore Case While Matching
+let myString = "freeCodeCamp";
+let fccRegex = /freeCodeCamp/i; // Change this line
+let result = fccRegex.test(myString);
+
+//Regular Expressions: Extract Matches
+let extractStr = "Extract the word 'coding' from this string.";
+let codingRegex = /coding/; // Change this line
+let result = extractStr.match(codingRegex); // Change this line
+
+//Regular Expressions: Match Anything with Wildcard Period
+let exampleSt = "let's have run with regular expression";
+let unRegex = /un./;
+let result = exampleSt.match(unRegex);
 
 */
+
+//Regular Expressions: Match Single Character with Multiple Possibilities
+
+
+console.log(result);
