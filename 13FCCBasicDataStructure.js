9d24b1450cb5f318a7e2d3a4807b5f35b3474b04
M IQBAL <rashel.edu@gmil.com>
2018-07-25 16:53:26 +0100

    mostafaIqbal

--- 13FCCBasicDataStructure.js
+++ 13FCCBasicDataStructure.js
@@ -2,7 +2,91 @@
 let array = ['a', 'b', 'c']
   array.push("king");
   array.unshift("queen");
-*/
-  let mixedNumbers = ['IV', 5, 'six']
+  // push() and unshift() Methods 
+  let array = ['IV', 5, 'six']
+      array.unshift('I', 2, 'Three');
+      array.push('seven', 'eight', 'nine')
 
-console.log(mixedNumbers);
+  //console.log(array);
+
+
+    function mixedNumbers(arr) {
+      arr.unshift('I', 2, 'Three');
+      arr.push(7, 'VIII', 9)
+      return arr;
+    }  
+  console.log(mixedNumbers(['IV', 5, 'six']));
+
+// pop() and shift () Mohods of Array 
+let array = ['IV', 5, 'six']
+array.pop();
+console.log(array);
+array.shift();
+console.log(array);
+
+// splice () Method : Removing and adding at the same time. 
+let arr = ['DarkGoldenRod', 'WhiteSmoke', 'LavenderBlush', 'PaleTurqoise', 'FireBrick'];
+arr.splice(arr,2, 'DarkSalmon', 'BlanchedAlmond');
+console.log(arr);
+
+// Slice() 
+function forecast(arr) {
+  // change code below this line
+  let newArr = arr.slice(2,4);
+  return newArr;
+}
+console.log(forecast(['cold', 'rainy', 'warm', 'sunny', 'cool', 'thunderstorms']));
+
+
+// Copy array with Spread operator
+function copyMachine(arr, num) {
+  let newArr = [];
+  while (num >= 1) {
+    // change code below this line
+    newArr = [[...arr], ...newArr];
+    // change code above this line
+    num--;
+  }
+  return newArr;
+}
+// change code here to test different cases:
+console.log(copyMachine([true, false, true], 4));
+
+//***Check For The Presence of an Element With indexOf()
+function quickCheck(arr, elem) {
+  if (arr.indexOf(elem) >= 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+console.log(
+  quickCheck([3, 5, 9, 125, 45, 2], 125)
+);  
+
+*/
+
+
+ 
+function filteredArray(arr, elem) {
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+
+      for (let y = 0; y < arr[i].length; y++) {
+
+        if (arr[i][y] !== elem){
+          newArr.push(arr[i]);
+        }
+      }
+        
+    }  
+  
+   return newArr;
+  }
+  
+  // change code here to test different cases:
+  console.log(
+    filteredArray ([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3)
+  );
+
+  
