a57a7f36fcdd4f4113e142cebcdeda5c4c6f62bc
M Iqbal <rashel.edu@gmail.com>
2018-08-09 18:34:33 +0100

    'win'

--- 14BasicAlg.js
+++ 14BasicAlg.js
@@ -17,3 +17,18 @@ function reverseString(str) {
 */
 
 // Factorialize a Number
+
+function factorialize(num) {
+    
+    if (num === 0 || num === 1)
+    return 1;
+
+    for (let i = num - 1; i >= 1; i--) {
+
+
+        num *= i;       
+    }
+    return num;
+  }
+
+console.log(factorialize(5));
