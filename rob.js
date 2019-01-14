79f3da34d6787e81010e03bd5906f58d35191370
rashel.edu@gmail.com <rashel.edu@gmail.com>
2019-01-14 13:20:49 +0000

    update

--- rob.js
+++ rob.js
@@ -10,10 +10,22 @@ document.getElementById("btn").onclick = function () {
 document.getElementById("btn").ondblclick = function() {
     document.getElementById("demo").innerHTML = " ";
 }
-*/
 
 //Append text
-
-document.getElementById("btn").onclick = function () {
+    document.getElementById("btn").onclick = function () {
     document.getElementById("demo").innerHTML = "I believe " + document.getElementById("sec").innerHTML + " awesom";
 }
+
+// manupulating style 
+
+    document.getElementById("btn").onclick = function() {
+        document.getElementById("demo").style.color = "red";
+        document.getElementById("demo").style.fontSize = "35px";
+        document.getElementById("btn").innerHTML = "Double Click to get normal font";
+    }
+    document.getElementById("btn").ondblclick = function() {
+        document.getElementById("demo").style.color = "white";
+        document.getElementById("demo").style.fontSize = "15px";
+        document.getElementById("btn").innerHTML = "Click here for bigger font";
+    }
+    */
